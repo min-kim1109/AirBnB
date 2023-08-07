4d05abc7986183326cd9f9d781eb2e61c8cfbef5
Min <minseok.km@gmail.com>
2023-08-07 16:15:45 -0400

    backend updates/render not deploying correctly

--- backend/db/seeders/20230804195721-demo-user.js
+++ backend/db/seeders/20230804195721-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Min',
+        lastName: 'Kim',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Person1FN',
+        lastName: 'Person1LN',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Person2FN',
+        lastName: 'Person2LN',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,54 +2,11 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
-
-// GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user: user });
-});
-
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
-
-router.use(restoreUser);
-
-router.get(
-    '/restore-user',
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
-
-
-// backend/routes/api/index.js
-// ...
-
-router.use(restoreUser);
-
-// ...
-
-// GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
-
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
@@ -60,5 +17,4 @@ router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
 
-// ...
 module.exports = router;
