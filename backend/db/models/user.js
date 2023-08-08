55dbd3c80fafe18cab5a2a1f2261792bb8ba6300
Min <minseok.km@gmail.com>
2023-08-08 17:02:01 -0400

    dev database push

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,6 +5,15 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // define association here
+      User.hasMany(models.Spot, {
+        foreignKey: 'ownerId'
+      })
+      User.hasMany(models.Review, {
+        foreignKey: 'userId'
+      })
+      User.hasMany(models.Booking, {
+        foreignKey: 'userId'
+      })
     }
   };
 
