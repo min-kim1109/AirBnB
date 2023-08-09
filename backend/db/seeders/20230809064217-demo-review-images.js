ce0ffd4e1d115c22a2ad30e795f6a76af0d543f3
Min <minseok.km@gmail.com>
2023-08-09 02:56:26 -0400

    final seeders #1

--- backend/db/seeders/20230809064217-demo-review-images.js
+++ backend/db/seeders/20230809064217-demo-review-images.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { reviewImage } = require('../models');
+const { ReviewImage } = require('../models');
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await reviewImage.bulkCreate([
+    await ReviewImage.bulkCreate([
       {
         reviewId: 1,
         url: 'www.test1'
