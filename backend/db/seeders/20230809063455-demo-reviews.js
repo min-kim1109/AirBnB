405f4b33cb6e07b9fcc242d7fbb68dba9245f38d
Min <minseok.km@gmail.com>
2023-08-09 02:40:38 -0400

    demo-review seeders added

---
+++ backend/db/seeders/20230809063455-demo-reviews.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const { Review } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await Review.bulkCreate([
+      {
+        spotId: 1,
+        userId: 1,
+        review: 'Pretty good',
+        stars: 4
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        review: 'Not good',
+        stars: 1
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        review: 'Could have been better, but not terrible',
+        stars: 3
+      }
+    ])
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableNmae = 'Reviews';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
