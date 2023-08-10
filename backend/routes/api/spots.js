0b6f595ff14d018c3d82b38f2832d316aa41d089
Min <minseok.km@gmail.com>
2023-08-10 14:19:09 -0400

    progress on spots routers

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,202 @@
+const express = require('express');
+const router = express.Router();
+const { Spot, Review, User, SpotImage } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+
+
+//GET ALL SPOTS
+router.get("/", requireAuth, async (req, res) => {
+    const userId = req.user.id
+    const spots = await Spot.findAll({
+        where: {
+            ownerId: userId
+        },
+        include: [
+            {
+                model: Review
+            },
+            {
+                model: SpotImage
+            }
+        ]
+    })
+
+
+    let spotsList = [];
+    spots.forEach(spot => {
+        spotsList.push(spot.toJSON())
+    })
+
+    spotsList.forEach(spot => {
+        spot.avgRating = 0
+        spot.Reviews.forEach(review => {
+            spot.avgRating += review.stars
+        })
+        spot.avgRating = spot.avgRating / spot.Reviews.length
+        delete spot.Reviews
+    })
+
+    spotsList.forEach(spot => {
+        spot.SpotImages.forEach(image => {
+            if (image.preview === true) {
+                spot.previewImage = image.url
+            }
+        })
+        if (!spot.previewImage) {
+            spot.previewImage = 'No preview image found'
+        }
+        delete spot.SpotImages
+    })
+
+    res.json(spotsList)
+})
+
+
+
+
+//CREATE A SPOT
+router.post('/', requireAuth, async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+    const spot = await Spot.create(
+        {
+            ownerId:
+                req.user.id,
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price
+        })
+    res.json(spot);
+})
+
+//EDIT A SPOT
+router.put("/:spotId", requireAuth, async (req, res, next) => {
+    const spotId = req.params.spotId;
+    const {
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price,
+        previewImage
+    } = req.body;
+    const spot = await Spot.findByPk(spotId);
+
+    if (!spot) {
+        res.status(404)
+        res.json({
+            message: "Spot couldn't be found"
+        })
+    }
+
+    const editedSpot = await spot.update({
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price,
+        previewImage
+    });
+    res.json(editedSpot)
+})
+
+
+//ADD AN IMAGE TO A SPOT BASED ON THE SPOT'S ID
+router.post('/:spotId/images', requireAuth, async (req, res) => {
+    const { url } = req.body;
+    const spot = await Spot.findByPk(req.params.spotId);
+
+    if (!spot) {
+        res.status(404)
+        res.json({
+            message: "Spot couldn't be found"
+        })
+    }
+
+    const spotImage = await SpotImage.create({
+        spotId: parseInt(req.params.spotId),
+        url: url,
+        preview: true
+    })
+
+    res.json(spotImage)
+})
+
+router.get('/:spotId', async (req, res) => {
+    const { spotId } = req.params;
+    const spot = await Spot.findByPk(spotId);
+
+    if (!spot) {
+        res
+            .status(404)
+            .json({
+                message: "Spot couldn't be found",
+                statusCode: 404
+            })
+    }
+
+    res.json(spot)
+})
+
+// GET ALL SPOTS OF CURRENT USERS
+router.get('/current', requireAuth, async (req, res) => {
+    const userId = req.user.id
+    const spots = await Spot.findAll({
+        where: {
+            ownerId: userId
+        },
+        includes: [
+            {
+                model: Review,
+            },
+            {
+                model: spotImage
+            }
+        ]
+    })
+
+    let spotsList = [];
+    spots.forEach(spot => {
+        spotsList.push(spot.toJSON())
+    })
+
+    spotsList.forEach(spot => {
+        spot.avgRating = 0
+        spot.Reviews.forEach(review => {
+            spot.avgRating += review.stars
+        })
+        spot.avgRating = spot.avgRating / spot.Reviews.length
+        delete spot.Reviews
+    })
+
+    spotsList.forEach(spot => {
+        spot.SpotImages.forEach(image => {
+            if (image.preview === true) {
+                spot.previewImage = image.url
+            }
+        })
+        if (!spot.previewImage) {
+            spot.previewImage = 'No preview image found'
+        }
+        delete spot.SpotImages
+    })
+
+    res.json(spotsList)
+})
+
+
+module.exports = router;
