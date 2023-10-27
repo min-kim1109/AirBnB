2208f45d19fead867e8ced3b9d68b5927ec051c6
Min <minseok.km@gmail.com>
2023-10-27 01:37:12 -0400

    demo reviews seeder updated

--- backend/db/seeders/4-demo-reviews.js
+++ backend/db/seeders/4-demo-reviews.js
@@ -13,44 +13,86 @@ module.exports = {
     await Review.bulkCreate([
       {
         spotId: 1,
-        userId: 1,
-        review: 'Decent',
+        userId: 2,
+        review: 'Decent stay',
+        stars: 5
+      },
+      {
+        spotId: 1,
+        userId: 2,
+        review: 'Loved my time here!',
+        stars: 4
+      },
+      {
+        spotId: 1,
+        userId: 3,
+        review: 'Professor Oak was a little overbearing',
+        stars: 2
+      },
+      {
+        spotId: 1,
+        userId: 4,
+        review: 'Someone took the starter I wanted!',
+        stars: 3
+      },
+      {
+        spotId: 1,
+        userId: 6,
+        review: 'Ash\'s mom was super welcoming, would book again',
         stars: 5
       },
       {
         spotId: 2,
-        userId: 1,
-        review: 'Great stay',
+        userId: 3,
+        review: 'Roxanne was a tough battle.',
         stars: 4
+      },
+      {
+        spotId: 2,
+        userId: 5,
+        review: 'Dev Corp architecture is awesome, definitely check it out',
+        stars: 5
       },
       {
         spotId: 3,
         userId: 2,
-        review: 'Horrible',
+        review: 'Monks made the stay a little awkward',
         stars: 3
+      },
+      {
+        spotId: 3,
+        userId: 7,
+        review: 'No AC, was cold at night...',
+        stars: 1
       },
       {
         spotId: 4,
         userId: 3,
-        review: 'Horrible',
+        review: 'Great, won it big at the gambling corner!',
         stars: 5
+      },
+      {
+        spotId: 4,
+        userId: 2,
+        review: 'I lost all my money.',
+        stars: 1
       },
       {
         spotId: 5,
         userId: 4,
-        review: 'Horrible',
+        review: 'Beat the Elite 4 and then lost to the champion, will try again next time.',
         stars: 4
       },
       {
         spotId: 6,
         userId: 5,
-        review: 'Horrible',
+        review: 'Horrible, think my room was haunted',
         stars: 1
       },
       {
         spotId: 7,
         userId: 6,
-        review: 'Horrible',
+        review: 'Had a good time!',
         stars: 4
       },
 
